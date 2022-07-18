a118e85b55efa5360e98ed97e3dbc5ffa98ba54b
pauldechorgnat <pdechorgnat@yahoo.fr>
2022-07-18 08:59:26 +0200

    Adding a __init__ file

---
+++ spatial_networks/__init__.py
@@ -0,0 +1,10 @@
+from spatial_networks.spatial_networks import SpatialNetwork
+from spatial_networks.spatial_networks import RandomGeometricGraph
+
+import warnings
+
+warnings.warn(
+    """
+    This package is a demonstration package (for now on). It has not been tested and should not be used for important tasks.
+    """
+)
